fa627536d297c8d604ca1dce5aa14433464b2d79
senbar <sendek.bartosz@gmail.com>
2019-01-11 09:47:19 +0100

    ulamek

---
+++ Ulamek/Ulamek/Ulamek/Fraction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulamek
+{
+    class Fraction
+    {
+        public int Numerator;
+        public int Denominator;
+
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new FormatException("denominator can not be 0");
+
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+
+        public static Fraction operator *(Fraction first, Fraction second)
+        {
+            return new Fraction(first.Numerator * second.Numerator, first.Denominator * second.Denominator);
+        }
+
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            return new Fraction(first.Numerator * second.Denominator + second.Numerator * first.Denominator,
+                first.Denominator * second.Denominator);
+        }
+    }
+}
---
+++ Ulamek/Ulamek/Ulamek/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulamek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction[] fractions= new Fraction[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                int inumerator, idenominator;
+                string numerator, denominator;
+
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine(String.Format("podaj {0} ulamek", i == 0 ? "pierwszy" : "drugi"));
+                        numerator = Console.ReadLine();
+                        denominator = Console.ReadLine();
+
+                        int z = 0;
+                    } while (!(
+                    int.TryParse(numerator, out inumerator)
+                    && int.TryParse(denominator, out idenominator))
+                    );
+                } while (idenominator == 0);
+
+                fractions[i] = new Fraction();
+            }
+
+            Console.WriteLine(String.Format("iloczyn tych ulamkow to {0}/{1}",(fractions[0]*fractions[1]).Numerator,
+                (fractions[0]*fractions[1]).Denominator));
+            
+
+        }
+    }
+}
