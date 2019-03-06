6da55f51fbe428b90d3236b6d7c416b19751ffa3
senbar <barsen97@gmail.com>
2019-03-06 16:09:19 +0100

    finish polynomial

--- Ulamek/Ulamek/Ulamek/Fraction.cs
+++ Ulamek/Ulamek/Ulamek/Fraction.cs
@@ -36,5 +36,55 @@ namespace Ulamek
             return new Fraction(first.Numerator * second.Denominator + second.Numerator * first.Denominator,
                 first.Denominator * second.Denominator);
         }
+        public static Fraction operator -(Fraction first, Fraction second)
+        {
+            second.Numerator *= -1;
+            return first + second;
+        }
+        public static Fraction operator /(Fraction first, Fraction second)
+        {
+            return new Fraction(first.Numerator * second.Denominator, first.Denominator * second.Numerator);
+        }
+        public static Fraction operator ++(Fraction f)
+        {
+            f.Numerator += f.Denominator;
+            return f;
+        }
+        public static Fraction operator --(Fraction f)
+        {
+            f.Numerator -= f.Denominator;
+            return f;
+        }
+        static int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+
+        public void Simplify()
+        {
+            int hGCD = GCD(this.Numerator, this.Denominator);
+            this.Numerator /= hGCD;
+            this.Denominator /= hGCD;
+        } 
+
+        public static bool operator ==(Fraction first, Fraction second)
+        {
+            first.Simplify();
+            second.Simplify();
+            return (first.Numerator == second.Numerator && first.Denominator == second.Denominator);
+        } 
+
+        public static bool operator !=(Fraction first, Fraction second)
+        {
+            return !(first == second);
+        }
+        public static bool operator true(Fraction f)
+        {
+            return f.Numerator != 0;
+        }
+        public static bool operator false(Fraction f)
+        {
+            return f.Numerator == 0;
+        }
     }
 }
---
+++ Wielomian/Polynomial/Polynomial/Polynomial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    class Polynomial
+    {
+        List<double> coords;
+        public Polynomial()
+        {
+            coords = new List<double>();
+        }
+        public Polynomial (int degree)
+        {
+            coords = new List<double>(degree);
+        }
+        public static Polynomial operator+(Polynomial p1, Polynomial p2)
+        {
+            if (p1.coords.Count > p2.coords.Count)
+                return (p2 + p1);
+            for(int i=0; i<p1.coords.Count; i++)
+            {
+                p2.coords[i] += p1.coords[i];
+            }
+            return p2;
+        }
+
+        public static Polynomial operator-(Polynomial p1, Polynomial p2)
+        {
+            for(int i=0; i<p2.coords.Count; i++)
+            {
+                p2.coords[i] *= -1;
+            }
+            return p1 + p2;
+        }
+        public static Polynomial operator *(Polynomial p1, Polynomial p2)
+        {
+            Polynomial result = new Polynomial(p1.coords.Count+ p2.coords.Count);
+            for(int i=0; i<p1.coords.Count; i++)
+            {
+                for(int j=0; j<p2.coords.Count; i++)
+                {
+                    result.coords[i + j] = p1.coords[i] + p2.coords[j];
+                }
+            }
+            return result;
+        }
+        public double Horner(double x)
+        {
+            double wynik = this.coords[0];
+            for (int i = 0; i < this.coords.Count; i++)
+                wynik = wynik * x + this.coords[i];
+            return wynik;
+                
+        }
+        
+    }
+}
