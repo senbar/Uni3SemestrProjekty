464ce37466e0cab575badbc3e370ee68156e91df
senbar <barsen97@gmail.com>
2019-03-06 14:27:21 +0100

    finished adres

---
+++ Adres/Adres/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adres
+{
+    class Person
+    {
+        public string City;
+        public string Street;
+        public int HouseNumber;
+
+        public Person(Person person)
+        {
+            this.City = person.City;
+            this.Street = person.Street;
+            this.HouseNumber = person.HouseNumber;
+        }
+        public Person()
+        {
+                                        
+        }
+    }
+}
---
+++ Adres/Adres/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person z = new Person();
+            z.City = "katowice";
+            z.Street = "adfs";
+            z.HouseNumber = 4;
+
+            Person s = new Person(z);
+            Console.Write(s.City +'\n' +s.Street + '\n' +s.HouseNumber.ToString());
+            Console.ReadKey();
+        }
+
+        
+    }
+}
+    
